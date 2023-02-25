564f25463417f496bfd5ab34fe7788185af1ed60
Code Sandbox <hello@codesandbox.io>
2023-02-25 06:06:46 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.17ca6916f67653ce5833383123f454b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9f1a039621944f5c2f6c7199018e15",
+    "url": "https://peketamin.github.io/csb-6x6z9z/index.html"
+  },
+  {
+    "revision": "beda89a392174df9e8ef",
+    "url": "https://peketamin.github.io/csb-6x6z9z/static/css/main.60036c3e.chunk.css"
+  },
+  {
+    "revision": "261967e13ab6e77bdf95",
+    "url": "https://peketamin.github.io/csb-6x6z9z/static/js/2.16a52a1f.chunk.js"
+  },
+  {
+    "revision": "beda89a392174df9e8ef",
+    "url": "https://peketamin.github.io/csb-6x6z9z/static/js/main.492cbb4e.chunk.js"
+  },
+  {
+    "revision": "732288e03a3323b65959",
+    "url": "https://peketamin.github.io/csb-6x6z9z/static/js/runtime~main.b90b6c9b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://peketamin.github.io/csb-6x6z9z/precache-manifest.17ca6916f67653ce5833383123f454b4.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://peketamin.github.io/csb-6x6z9z/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
